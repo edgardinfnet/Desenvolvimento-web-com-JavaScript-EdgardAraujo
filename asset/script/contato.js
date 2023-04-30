a7f5a569b85ee4103614177033c9575b8d116607
edgardinfnet <53659466+edgardinfnet@users.noreply.github.com>
2023-04-30 18:12:08 +0000

    voltando para a versão inicial do formulário com validação no HTML

--- asset/script/contato.js
+++ asset/script/contato.js
@@ -11,78 +11,79 @@ formInputs.forEach((f) => {
   f.classList.add("inputs-border");
 });
 
-contatoForm.onsubmit = (event) => {
-  event.preventDefault();
+// validação será ajustada no futuro bem próximo
+// contatoForm.onsubmit = (event) => {
+//   event.preventDefault();
 
-  const nomeValor = nomeForm.value;
-  const telefoneValor = telefoneForm.value;
-  const emailValor = emailForm.value;
-  const profissionalValor = profissionalForm.value;
-  const mensagemValor = mensagemForm.value;
-  const urgenciaValor = urgenciaForm.value;
+//   const nomeValor = nomeForm.value;
+//   const telefoneValor = telefoneForm.value;
+//   const emailValor = emailForm.value;
+//   const profissionalValor = profissionalForm.value;
+//   const mensagemValor = mensagemForm.value;
+//   const urgenciaValor = urgenciaForm.value;
 
-  if (isValidNome(nomeValor)) {
-    console.log("nomeValor: ", nomeValor);
-  } else {
-    console.log("nomeValor: ", "inválido");
-  }
+//   if (isValidNome(nomeValor)) {
+//     console.log("nomeValor: ", nomeValor);
+//   } else {
+//     console.log("nomeValor: ", "inválido");
+//   }
 
-  if (isValidEmail(emailValor)) {
-    console.log("emailValor: ", emailValor);
-  } else {
-    console.log("emailValor: ", "inválido");
-  }
+//   if (isValidEmail(emailValor)) {
+//     console.log("emailValor: ", emailValor);
+//   } else {
+//     console.log("emailValor: ", "inválido");
+//   }
 
-  if (isValidMessagem(mensagemValor)) {
-    console.log("mensagemValor: ", mensagemValor);
-  } else {
-    console.log("mensagemValor: ", "inválido");
-  }
+//   if (isValidMessagem(mensagemValor)) {
+//     console.log("mensagemValor: ", mensagemValor);
+//   } else {
+//     console.log("mensagemValor: ", "inválido");
+//   }
 
-  console.log("telefoneValor: ", telefoneValor);
-  console.log("profissionalValor: ", profissionalValor);
-  console.log("urgenciaValor: ", urgenciaValor);
+//   console.log("telefoneValor: ", telefoneValor);
+//   console.log("profissionalValor: ", profissionalValor);
+//   console.log("urgenciaValor: ", urgenciaValor);
 
-};
+// };
 
-function isValidNome(value) {
-  if (value.length < 4) {
-    return false;
-  } else if (value.length > 36) {
-    return false;
-  }
-  return true;
-}
+// function isValidNome(value) {
+//   if (value.length < 4) {
+//     return false;
+//   } else if (value.length > 36) {
+//     return false;
+//   }
+//   return true;
+// }
 
-function isValidTelefone(value) {
+// function isValidTelefone(value) {
 
-}
+// }
 
-function isValidEmail(value) {
-  const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+// function isValidEmail(value) {
+//   const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-  const validEmail = value.match(regexEmail);
+//   const validEmail = value.match(regexEmail);
 
-  if (validEmail === null) {
-    return false;
-  } else {
-    return true;
-  }
-}
+//   if (validEmail === null) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }
 
-function isValidProfissional(value) {
+// function isValidProfissional(value) {
 
-}
+// }
 
-function isValidMessagem(value) {
-  if (value.length < 30) {
-    return false;
-  } else if (value.length > 300) {
-    return false;
-  }
-  return true;
-}
+// function isValidMessagem(value) {
+//   if (value.length < 30) {
+//     return false;
+//   } else if (value.length > 300) {
+//     return false;
+//   }
+//   return true;
+// }
 
-function isValidUrgencia(value) {
+// function isValidUrgencia(value) {
 
-}
+// }
